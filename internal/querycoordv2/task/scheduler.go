22df6fc7e4987e7cbbe66e78ee8d51418f35ac29
yah01 <yang.cen@zilliz.com>
2022-07-20 16:30:35 +0800

    Use type switch assertion
    Signed-off-by: yah01 <yang.cen@zilliz.com>

--- internal/querycoordv2/task/scheduler.go
+++ internal/querycoordv2/task/scheduler.go
@@ -77,12 +77,12 @@ func (scheduler *Scheduler) Add(task Task) error {
 
 	task.SetID(scheduler.idAllocator())
 
-	switch task.(type) {
+	switch task := task.(type) {
 	case *SegmentTask:
-		scheduler.segmentTasks[task.(*SegmentTask).segmentID] = task
+		scheduler.segmentTasks[task.segmentID] = task
 
 	case *ChannelTask:
-		scheduler.channelTasks[task.(*ChannelTask).channel] = task
+		scheduler.channelTasks[task.channel] = task
 	}
 
 	return nil
@@ -91,9 +91,9 @@ func (scheduler *Scheduler) Add(task Task) error {
 // check checks whether the task is valid to add,
 // must hold lock
 func (scheduler *Scheduler) preAdd(task Task) error {
-	switch task.(type) {
+	switch task := task.(type) {
 	case *SegmentTask:
-		segmentID := task.(*SegmentTask).segmentID
+		segmentID := task.segmentID
 		if old, ok := scheduler.segmentTasks[segmentID]; ok {
 			log.Warn("failed to add task, a task processing the same segment existed",
 				zap.Int64("old-id", old.ID()),
@@ -103,7 +103,7 @@ func (scheduler *Scheduler) preAdd(task Task) error {
 		}
 
 	case *ChannelTask:
-		channel := task.(*ChannelTask).channel
+		channel := task.channel
 		if old, ok := scheduler.channelTasks[channel]; ok {
 			log.Warn("failed to add task, a task processing the same channel existed",
 				zap.Int64("old-id", old.ID()),
@@ -210,9 +210,9 @@ func (scheduler *Scheduler) checkStale(task Task) bool {
 			return true
 		}
 
-		switch action.(type) {
+		switch action := action.(type) {
 		case *SegmentAction:
-			segmentID := action.(*SegmentAction).SegmentID()
+			segmentID := action.SegmentID()
 			if !scheduler.targetMgr.ContainSegment(segmentID) {
 				log.Warn("the task is stale, the task's segment not exists",
 					zap.Int64("segment-id", segmentID))
@@ -221,7 +221,7 @@ func (scheduler *Scheduler) checkStale(task Task) bool {
 			}
 
 		case *DmChannelAction:
-			channel := action.(*DmChannelAction).ChannelName()
+			channel := action.ChannelName()
 			if !scheduler.targetMgr.ContainDmChannel(channel) {
 				log.Warn("the task is stale, the task's channel not exists",
 					zap.String("channel-name", channel))
