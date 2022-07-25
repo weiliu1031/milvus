0c4500d8c73e06caed6752f393f6159ca76ed902
yah01 <yang.cen@zilliz.com>
2022-07-25 11:11:06 +0800

    CollectionManager persists data
    Signed-off-by: yah01 <yang.cen@zilliz.com>

--- internal/querycoordv2/meta/collection_manager.go
+++ internal/querycoordv2/meta/collection_manager.go
@@ -3,6 +3,7 @@ package meta
 import (
 	"sync"
 
+	"github.com/milvus-io/milvus/internal/kv"
 	"github.com/milvus-io/milvus/internal/proto/schemapb"
 	. "github.com/milvus-io/milvus/internal/util/typeutil"
 )
@@ -19,12 +20,22 @@ type CollectionManager struct {
 	rwmutex sync.RWMutex
 
 	collections map[UniqueID]*Collection
+	store       Store
+
+	broker *CoordinatorBroker
 }
 
-func NewCollectionManager() *CollectionManager {
+func NewCollectionManager(txnKV kv.TxnKV, broker *CoordinatorBroker) *CollectionManager {
 	return &CollectionManager{
 		collections: make(map[int64]*Collection),
+		store:       NewMetaStore(txnKV),
+
+		broker: broker,
 	}
+}
+
+func (m *CollectionManager) Reload() {
+	// TODO(yah01)
 }
 
 func (m *CollectionManager) Get(id UniqueID) *Collection {
@@ -34,20 +45,33 @@ func (m *CollectionManager) Get(id UniqueID) *Collection {
 	return m.collections[id]
 }
 
-func (m *CollectionManager) Put(collections ...*Collection) {
+func (m *CollectionManager) Put(collection *Collection) error {
 	m.rwmutex.Lock()
 	defer m.rwmutex.Unlock()
 
-	for _, collection := range collections {
-		m.collections[collection.CollectionID] = collection
+	err := m.store.Load(collection.CollectionID, collection.Partitions)
+	if err != nil {
+		return err
 	}
+	m.collections[collection.CollectionID] = collection
+
+	return nil
 }
 
-func (m *CollectionManager) Remove(ids ...UniqueID) {
+func (m *CollectionManager) Remove(id UniqueID) error {
 	m.rwmutex.Lock()
 	defer m.rwmutex.Unlock()
 
-	for _, id := range ids {
-		delete(m.collections, id)
+	collection, ok := m.collections[id]
+	if !ok {
+		return nil
 	}
+
+	err := m.store.Release(collection.CollectionID, collection.Partitions)
+	if err != nil {
+		return err
+	}
+	delete(m.collections, id)
+
+	return nil
 }
