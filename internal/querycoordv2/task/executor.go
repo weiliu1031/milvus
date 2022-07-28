d69e8b7b44889220578b433f35eca15e0f125034
yah01 <yang.cen@zilliz.com>
2022-07-29 00:50:16 +0800

    refactor execution of segment action
    Signed-off-by: yah01 <yang.cen@zilliz.com>

--- internal/querycoordv2/task/executor.go
+++ internal/querycoordv2/task/executor.go
@@ -24,11 +24,12 @@ type actionIndex struct {
 }
 
 type Executor struct {
-	meta    *meta.Meta
-	dist    *meta.DistributionManager
-	broker  *meta.CoordinatorBroker
-	cluster *session.Cluster
-	nodeMgr *session.NodeManager
+	meta      *meta.Meta
+	dist      *meta.DistributionManager
+	broker    *meta.CoordinatorBroker
+	targetMgr *meta.TargetManager
+	cluster   *session.Cluster
+	nodeMgr   *session.NodeManager
 
 	executingActions sync.Map
 }
@@ -36,14 +37,16 @@ type Executor struct {
 func NewExecutor(meta *meta.Meta,
 	dist *meta.DistributionManager,
 	broker *meta.CoordinatorBroker,
+	targetMgr *meta.TargetManager,
 	cluster *session.Cluster,
 	nodeMgr *session.NodeManager) *Executor {
 	return &Executor{
-		meta:    meta,
-		dist:    dist,
-		broker:  broker,
-		cluster: cluster,
-		nodeMgr: nodeMgr,
+		meta:      meta,
+		dist:      dist,
+		broker:    broker,
+		targetMgr: targetMgr,
+		cluster:   cluster,
+		nodeMgr:   nodeMgr,
 
 		executingActions: sync.Map{},
 	}
@@ -92,6 +95,23 @@ func (ex *Executor) Execute(task Task, step int, action Action) bool {
 }
 
 func (ex *Executor) executeSegmentAction(task *SegmentTask, action *SegmentAction) {
+	switch action.Type() {
+	case ActionTypeGrow:
+		ex.loadSegment(task, action)
+
+	case ActionTypeReduce:
+		ex.releaseSegment(task, action)
+
+	default:
+		panic(fmt.Sprintf("invalid action type: %+v", action.Type()))
+	}
+}
+
+func (ex *Executor) loadSegment(task *SegmentTask, action *SegmentAction) {
+	if action.Type() != ActionTypeGrow {
+		panic("load segment action type not matched")
+	}
+
 	log := log.With(
 		zap.Int64("msg-id", task.MsgID()),
 		zap.Int64("task-id", task.ID()),
@@ -104,74 +124,115 @@ func (ex *Executor) executeSegmentAction(task *SegmentTask, action *SegmentActio
 	ctx, cancel := context.WithTimeout(task.Context(), actionTimeout)
 	defer cancel()
 
-	switch action.Type() {
-	case ActionTypeGrow:
-		collection := ex.meta.CollectionManager.Get(task.CollectionID())
-		if collection == nil {
-			log.Warn("failed to get collection")
-			return
-		}
+	collection := ex.meta.CollectionManager.Get(task.CollectionID())
+	if collection == nil {
+		log.Warn("failed to get collection")
+		return
+	}
 
-		segment, err := ex.broker.GetSegmentInfo(ctx, task.segmentID)
-		if err != nil {
-			log.Warn("failed to get segment info from DataCoord", zap.Error(err))
-			return
-		}
-		indexes, err := ex.broker.GetIndexInfo(ctx, collection.Schema, collection.CollectionID, segment.ID)
-		if err != nil {
-			log.Warn("failed to get index of segment, will load without index")
-		}
-		loadInfo := packSegmentLoadInfo(segment, indexes)
+	segment, err := ex.broker.GetSegmentInfo(ctx, task.segmentID)
+	if err != nil {
+		log.Warn("failed to get segment info from DataCoord", zap.Error(err))
+		return
+	}
+	indexes, err := ex.broker.GetIndexInfo(ctx, collection.Schema, collection.CollectionID, segment.ID)
+	if err != nil {
+		log.Warn("failed to get index of segment, will load without index")
+	}
+	loadInfo := packSegmentLoadInfo(segment, indexes)
 
-		// Get shard leader for the given replica and segment
-		replica := ex.meta.ReplicaManager.GetByCollectionAndNode(task.CollectionID(), action.Node())
-		leader, ok := ex.dist.GetShardLeader(replica, segment.GetInsertChannel())
-		if !ok {
-			log.Warn("no shard leader for the segment to execute loading", zap.String("shard", segment.InsertChannel))
-			return
-		}
-		log = log.With(zap.Int64("shard-leader", leader))
+	// Get shard leader for the given replica and segment
+	replica := ex.meta.ReplicaManager.GetByCollectionAndNode(task.CollectionID(), action.Node())
+	if replica == nil {
+		log.Warn("failed to get replica for given collection and node")
+		return
+	}
+	leader, ok := ex.dist.GetShardLeader(replica, segment.GetInsertChannel())
+	if !ok {
+		log.Warn("no shard leader for the segment to execute loading", zap.String("shard", segment.GetInsertChannel()))
+		return
+	}
+	log = log.With(zap.Int64("shard-leader", leader))
 
-		// Pre-allocate memory for loading segment
-		node := ex.nodeMgr.Get(action.Node())
-		if node == nil {
-			log.Warn("failed to get node, the task may be stale")
-			return
-		}
-		ok, release := node.PreAllocate(loadInfo.SegmentSize)
-		if !ok {
-			log.Warn("no enough memory to pre-allocate for loading segment",
-				zap.Int64("node-memory-remaining", node.Remaining()),
-				zap.Int64("segment-size", loadInfo.SegmentSize))
-			return
-		}
-		defer release()
+	// Pre-allocate memory for loading segment
+	node := ex.nodeMgr.Get(action.Node())
+	if node == nil {
+		log.Warn("failed to get node, the task may be stale")
+		return
+	}
+	ok, release := node.PreAllocate(loadInfo.SegmentSize)
+	if !ok {
+		log.Warn("no enough memory to pre-allocate for loading segment",
+			zap.Int64("node-memory-remaining", node.Remaining()),
+			zap.Int64("segment-size", loadInfo.SegmentSize))
+		return
+	}
+	defer release()
 
-		req := packLoadSegmentRequest(task, action, collection, loadInfo)
-		status, err := ex.cluster.LoadSegments(ctx, leader, req)
-		if err != nil {
-			log.Warn("failed to load segment, it may be a false failure", zap.Error(err))
-			return
-		}
-		if status.ErrorCode != commonpb.ErrorCode_Success {
-			log.Warn("failed to load segment", zap.String("reason", status.GetReason()))
-			return
-		}
+	req := packLoadSegmentRequest(task, action, collection, loadInfo)
+	status, err := ex.cluster.LoadSegments(ctx, leader, req)
+	if err != nil {
+		log.Warn("failed to load segment, it may be a false failure", zap.Error(err))
+		return
+	}
+	if status.ErrorCode != commonpb.ErrorCode_Success {
+		log.Warn("failed to load segment", zap.String("reason", status.GetReason()))
+		return
+	}
+}
 
-	case ActionTypeReduce:
-		req := packReleaseSegmentRequest(task, action)
-		status, err := ex.cluster.ReleaseSegments(ctx, action.Node(), req)
-		if err != nil {
-			log.Warn("failed to release segment, it may be a false failure", zap.Error(err))
-			return
-		}
-		if status.ErrorCode != commonpb.ErrorCode_Success {
-			log.Warn("failed to release segment", zap.String("reason", status.GetReason()))
-			return
-		}
+func (ex *Executor) releaseSegment(task *SegmentTask, action *SegmentAction) {
+	if action.Type() != ActionTypeReduce {
+		panic("release segment action type not matched")
+	}
 
-	default:
-		panic(fmt.Sprintf("invalid action type: %+v", action.Type()))
+	log := log.With(
+		zap.Int64("msg-id", task.MsgID()),
+		zap.Int64("task-id", task.ID()),
+		zap.Int64("collection-id", task.CollectionID()),
+		zap.Int64("replica-id", task.ReplicaID()),
+		zap.Int64("segment-id", task.segmentID),
+		zap.Int64("node-id", action.Node()),
+	)
+
+	ctx, cancel := context.WithTimeout(task.Context(), actionTimeout)
+	defer cancel()
+
+	var targetSegment *meta.Segment
+	segments := ex.dist.SegmentDistManager.GetByNode(action.Node())
+	for _, segment := range segments {
+		if segment.ID == task.SegmentID() {
+			targetSegment = segment
+			break
+		}
+	}
+	if targetSegment == nil {
+		log.Warn("segment to release not found in distribution")
+		return
+	}
+
+	// Get shard leader for the given replica and segment
+	replica := ex.meta.ReplicaManager.GetByCollectionAndNode(task.CollectionID(), action.Node())
+	if replica == nil {
+		log.Warn("failed to get replica for given collection and node")
+		return
+	}
+	leader, ok := ex.dist.GetShardLeader(replica, targetSegment.GetInsertChannel())
+	if !ok {
+		log.Warn("no shard leader for the segment to execute loading", zap.String("shard", targetSegment.GetInsertChannel()))
+		return
+	}
+	log = log.With(zap.Int64("shard-leader", leader))
+
+	req := packReleaseSegmentRequest(task, action)
+	status, err := ex.cluster.ReleaseSegments(ctx, leader, req)
+	if err != nil {
+		log.Warn("failed to release segment, it may be a false failure", zap.Error(err))
+		return
+	}
+	if status.ErrorCode != commonpb.ErrorCode_Success {
+		log.Warn("failed to release segment", zap.String("reason", status.GetReason()))
+		return
 	}
 }
 
