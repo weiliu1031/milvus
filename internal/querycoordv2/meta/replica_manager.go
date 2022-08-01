eb11299211ba5b1e7bc4fd39878a7fce83354fa4
yah01 <yang.cen@zilliz.com>
2022-08-01 12:31:01 +0800

    ReplicaManager assigns nodes to replicas
    Signed-off-by: yah01 <yang.cen@zilliz.com>

--- internal/querycoordv2/meta/replica_manager.go
+++ internal/querycoordv2/meta/replica_manager.go
@@ -1,8 +1,10 @@
 package meta
 
 import (
+	"sort"
 	"sync"
 
+	"github.com/milvus-io/milvus/internal/querycoordv2/session"
 	. "github.com/milvus-io/milvus/internal/util/typeutil"
 )
 
@@ -16,7 +18,9 @@ type Replica struct {
 type ReplicaManager struct {
 	rwmutex sync.RWMutex
 
-	replicas map[UniqueID]*Replica
+	idAllocator func() (int64, error)
+	nodeMgr     *session.NodeManager
+	replicas    map[UniqueID]*Replica
 }
 
 func NewReplicaManager() *ReplicaManager {
@@ -32,10 +36,41 @@ func (m *ReplicaManager) Get(id UniqueID) *Replica {
 	return m.replicas[id]
 }
 
-func (m *ReplicaManager) Put(replicas ...*Replica) {
+func (m *ReplicaManager) Put(replicaNumber int32, collectionID UniqueID, partitionIDs ...UniqueID) ([]*Replica, error) {
+	var (
+		replicas = make([]*Replica, replicaNumber)
+		err      error
+	)
+	for i := range replicas {
+		replicas[i], err = m.spawn(collectionID, partitionIDs...)
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	m.rwmutex.Lock()
 	defer m.rwmutex.Unlock()
 
+	m.put(replicas...)
+	m.assignCollectionReplicas(replicas...)
+
+	return replicas, nil
+}
+
+func (m *ReplicaManager) spawn(collectionID UniqueID, partitionIDs ...UniqueID) (*Replica, error) {
+	id, err := m.idAllocator()
+	if err != nil {
+		return nil, err
+	}
+	return &Replica{
+		ID:           id,
+		CollectionID: collectionID,
+		PartitionIDs: NewUniqueSet(partitionIDs...),
+		Nodes:        make(UniqueSet),
+	}, nil
+}
+
+func (m *ReplicaManager) put(replicas ...*Replica) {
 	for _, replica := range replicas {
 		m.replicas[replica.ID] = replica
 	}
@@ -47,6 +82,53 @@ func (m *ReplicaManager) Remove(ids ...UniqueID) {
 
 	for _, id := range ids {
 		delete(m.replicas, id)
+	}
+}
+
+// Assign assigns nodes to the given replicas,
+// all given replicas must be the same collection,
+// tries to assign nodes to all replicas if no given replica
+func (m *ReplicaManager) Assign(replicaIDs ...UniqueID) {
+	m.rwmutex.Lock()
+	defer m.rwmutex.Unlock()
+
+	if len(replicaIDs) == 0 {
+		m.assignAllReplicas()
+	} else {
+		replicas := make([]*Replica, len(replicaIDs))
+		for i := range replicas {
+			replicas[i] = m.replicas[replicaIDs[i]]
+		}
+		m.assignCollectionReplicas(replicas...)
+	}
+}
+
+func (m *ReplicaManager) assignCollectionReplicas(replicas ...*Replica) {
+	replicaNumber := len(replicas)
+	nodes := m.nodeMgr.GetAll()
+	sort.Slice(nodes, func(i, j int) bool {
+		return nodes[i].GetScore() < nodes[j].GetScore()
+	})
+
+	for i, node := range nodes {
+		replicas[i%replicaNumber].Nodes.Insert(node.ID())
+	}
+}
+
+func (m *ReplicaManager) assignAllReplicas() {
+	panic("not implemented")
+}
+
+// RemoveCollection removes replicas of given collection,
+// this operation is slower than Remove, use Remove if the replicas' ID is known
+func (m *ReplicaManager) RemoveCollection(collectionID UniqueID) {
+	m.rwmutex.Lock()
+	defer m.rwmutex.Unlock()
+
+	for id, replica := range m.replicas {
+		if replica.CollectionID == collectionID {
+			delete(m.replicas, id)
+		}
 	}
 }
 
