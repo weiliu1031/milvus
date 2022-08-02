ebb10c1e5f09b1702b6a9f1b04ac34d5cb26e1c5
yah01 <yang.cen@zilliz.com>
2022-08-02 17:34:22 +0800

    Add leader distribution for DistributionManager
    Signed-off-by: yah01 <yang.cen@zilliz.com>

--- internal/querycoordv2/meta/dist_manager.go
+++ internal/querycoordv2/meta/dist_manager.go
@@ -1,13 +1,27 @@
 package meta
 
+type distributionManager struct {
+	*SegmentDistManager
+	*ChannelDistManager
+}
+
+func newDistributionManager() *distributionManager {
+	return &distributionManager{
+		SegmentDistManager: NewSegmentDistManager(),
+		ChannelDistManager: NewChannelDistManager(),
+	}
+}
+
 type DistributionManager struct {
 	*SegmentDistManager
 	*ChannelDistManager
+	LeaderDistribution *distributionManager
 }
 
 func NewDistributionManager() *DistributionManager {
 	return &DistributionManager{
 		SegmentDistManager: NewSegmentDistManager(),
 		ChannelDistManager: NewChannelDistManager(),
+		LeaderDistribution: newDistributionManager(),
 	}
 }
